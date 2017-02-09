9aa0a23b2245fe7a19b2b47ffe8b2520235ac40b
Philipp Schlegel <observing@web.de>
2017-02-09 15:10:29 +0000

    Added license information

--- pybot.py
+++ pybot.py
@@ -1,3 +1,21 @@
+"""
+    Pybot is a Slack bot that interfaces with CATMAID and ZOTERO
+    Copyright (C) 2017 Philipp Schlegel
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import time, re, threading, random, json, sys
 import matplotlib.pyplot as plt
 import rpy2.robjects as robjects
@@ -178,7 +196,13 @@ class return_zotero(threading.Thread):
 		tags = command.split(' ')
 
 		if '' in tags:
-			tags.remove('')		
+			tags.remove('')	
+
+		if 'file' in tags:
+			dl_file = True
+			tags.remove('file')
+		else:
+			dl_file = False
 
 		#Retrieve all items in library
 		items = zot.items()
