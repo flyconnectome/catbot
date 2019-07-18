bb58630b0cf42d19c3cbdb4db4853738c3747cce
Philipp Schlegel <observing@web.de>
2019-07-18 14:46:34 +0100

    fix botconfig.py

--- botconfig.py
+++ botconfig.py
@@ -1,27 +1,18 @@
-#General parameters
+# General parameters
 BOT_NAME = 'catbot'
-BOT_ID = ''
-AT_BOT = '<@' + BOT_ID + '>'
-READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
-MAX_PARALLEL_REQUESTS = 10 # not more than 10 threads at any given time
+BOT_USER_OAUTH_ACCESS_TOKEN = ''
+MAX_PARALLEL_REQUESTS = 10  # not more than 10 threads at any given time
 
-#Catmaid credentials
-SERVER_URL = ''
-AUTHTOKEN = ''
-HTTP_USER = ''
-HTTP_PW = ''
-PROJECT_ID =
+# Catmaid credentials
+CATMAID_SERVER_URL = ''
+CATMAID_AUTHTOKEN = ''
+CATMAID_HTTP_USER = ''
+CATMAID_HTTP_PW = ''
+CATMAID_PROJECT_ID = 1
 
-#Slack credentials
-SLACK_KEY = ''
+# Nblast databases
+R_FLYCIRCUIT_DB = ''
+R_JANELIA_GMR_DB = ''
 
-#Zotero credentials
-ZOT_KEY = ''
-ZOT_GRP_ID = ''
-
-#Nblast databases
-FLYCIRCUIT_DB = ''
-JANELIA_GMR_DB = ''
-
-# Folder with FAFB nightly dumps
+# Folder with FAFB nightly neuron dump
 FAFB_DUMP = ''
